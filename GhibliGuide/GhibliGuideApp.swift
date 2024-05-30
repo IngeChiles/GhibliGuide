7565fdef14d27c7a3e534cf97ace56b2d2658606
Inge Chiles <Inge.Chiles@gmail.com>
2024-05-30 15:14:05 -0500

    Initial Commit

---
+++ GhibliGuide/GhibliGuideApp.swift
@@ -0,0 +1,14 @@
+//  GhibliGuideApp.swift
+//  GhibliGuide
+//  Made with ♥︎ by Inge Chiles on 5/30/24.
+
+import SwiftUI
+
+@main
+struct GhibliGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
